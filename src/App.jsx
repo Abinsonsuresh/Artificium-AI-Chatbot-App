8dd499f256fc00d6788d31a1db129184ee736f9f
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-19 01:54:55 +0530

    Page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Header from "./Components/Header"
 import SideBar from "./Components/SideBar"
+import Artificium from "./Sections/Artificium"
 
 function App() {
 
   return (
     <>
       <div className="flex p-4 gap-5 max-h-screen overflow-hidden">
-        <SideBar/>
-        <Header />
+        <SideBar />
+        <div className="flex flex-col w-full">
+          <Header />
+          <Artificium />
+        </div>
       </div>
 
     </>
--- src/Sections/Artificium.jsx
+++ src/Sections/Artificium.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
+import { PiChatCircleLight } from 'react-icons/pi'
 
 const Artificium = () => {
-  return (
-    <div>Artificium</div>
-  )
+    return (
+        <>
+            <div className="flex justify-center items-center h-[600px] border mt-2 overflow-y-scroll  overflow-x-hidden scroll  scroll-smooth">
+                <div className='text-center flex flex-col items-center'>
+                    <div>
+                        <h3 className='font-bold text-xl'>Innovation Starter Pack</h3>
+                        <p className='text-sm text-gray-500'>Kickstart your innovation process with our comprehensive selection of predefined prompts.</p>
+                    </div>
+
+                    <div className="flex flex-row items-center gap-5 mt-6">
+                        <div className='flex flex-col items-center'>
+                            <div className='flex items-center flex-col'>
+                                <div className='bg-slate-900 p-4 rounded-full'>
+                                    <PiChatCircleLight />
+                                </div>
+                                <span>Creative Assets</span>
+                            </div>
+
+                            <div className='bg-gray-700 p-4 w-48 mt-6 rounded-2xl flex justify-between items-center'>
+                                <div>
+                                    <p>UI wireframe</p>
+                                </div>
+                                <PiChatCircleLight />
+                            </div>
+                        </div>
+
+
+                        <div className='flex flex-col items-center'>
+                            <div className='flex items-center flex-col'>
+                                <div className='bg-slate-900 p-4 rounded-full'>
+                                    <PiChatCircleLight />
+                                </div>
+                                <span>Creative Assets</span>
+                            </div>
+
+                            <div className='bg-gray-700 p-4 w-48 mt-6 rounded-2xl flex justify-between items-center'>
+                                <div>
+                                    <p>UI wireframe</p>
+                                </div>
+                                <PiChatCircleLight />
+                            </div>
+                        </div>
+
+
+                        <div className='flex flex-col items-center'>
+                            <div className='flex items-center flex-col'>
+                                <div className='bg-slate-900 p-4 rounded-full'>
+                                    <PiChatCircleLight />
+                                </div>
+                                <span>Creative Assets</span>
+                            </div>
+
+                            <div className='bg-gray-700 p-4 w-48 mt-6 rounded-2xl flex justify-between items-center'>
+                                <div>
+                                    <p>UI wireframe</p>
+                                </div>
+                                <PiChatCircleLight />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Artificium
