ff17fcb2d1bded13621126e32a70791eec8cb66f
Abinsonsuresh <polocreations916@gmail.com>
2024-03-19 19:44:19 +0530

    Nav section function added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,42 @@ import { useState } from "react"
 import Header from "./Components/Header"
 import SideBar from "./Components/SideBar"
 import Artificium from "./Sections/Artificium"
+import Library from "./Sections/Library"
+import Chat from "./Sections/Chat"
+
+
 import Share from "./Components/Share"
+
 
 function App() {
   const [share, setShare] = useState(false)
+  const [section, setSection] = useState("artificium")
+
+  let sectionComponent;
+
+  switch (section) {
+    case "artificium":
+      sectionComponent = <Artificium />;
+      break;
+      case "chat":
+      sectionComponent = <Chat />;
+      break;
+
+      case "library":
+      sectionComponent = <Library />;
+      break;
+    default:
+      sectionComponent = null;
+  }
+
+
   return (
     <>
       <div className="flex p-4 gap-5 max-h-screen  relative">
         <SideBar setShare={setShare} />
         <div className="flex flex-col">
           <Header />
-          <Artificium />
+          {sectionComponent}
         </div>
 
         {
