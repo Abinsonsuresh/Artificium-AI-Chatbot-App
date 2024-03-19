e9952e0e02a2c9c4d1fe7bc0fc133fa62ba54e8d
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-19 23:47:01 +0530

    Gemini API Fn integrated

---
+++ src/config/gemini.js
@@ -0,0 +1,58 @@
+const apiKey = "AIzaSyDBat01CZBfkwpaF2akx62h4eVp65bJnLA"
+
+
+// node --version # Should be >= 18
+// npm install @google/generative-ai
+
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+  
+  const MODEL_NAME = "gemini-1.0-pro";
+  const API_KEY = "AIzaSyDBat01CZBfkwpaF2akx62h4eVp65bJnLA";
+  
+  async function runChat(prompt) {
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  
+    const generationConfig = {
+      temperature: 0.9,
+      topK: 1,
+      topP: 1,
+      maxOutputTokens: 2048,
+    };
+  
+    const safetySettings = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+    ];
+  
+    const chat = model.startChat({
+      generationConfig,
+      safetySettings,
+      history: [
+      ],
+    });
+  
+    const result = await chat.sendMessage(prompt);
+    const response = result.response;
+    console.log(response.text());
+  }
+  
+  export default runChat;
