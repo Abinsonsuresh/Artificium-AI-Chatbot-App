23733fb7f7a8025b1d665fce4352ce2689334309
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-20 01:59:44 +0530

    loader added

--- src/Sections/Chat.jsx
+++ src/Sections/Chat.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { PiArrowFatLinesRight } from "react-icons/pi";
 import { useGeminiContext } from '../context/GeminiContext';
-import { IoCopyOutline } from "react-icons/io5";
+import { IoCopyOutline } from "react-icons/io5"
+import LoadingGIF from  '../assets/Loading.gif'
 const Chat = () => {
   const { onSent, input, setInput, recentPrompt, setRecentPrompt, prevPrompts, setPrevPrompts, showResult, loading, data } = useGeminiContext()
   return (
@@ -10,7 +11,7 @@ const Chat = () => {
         <div className='text-center w-full flex flex-col items-center h-full'>
 
           {/* CONTENT */}
-          <div className='flex w-full  flex-col items-center mt-8 gap-5'>
+          <div className='flex w-full  flex-col items-center mt-2 gap-5'>
 
             <div className="user bg-[#0D0F10] flex  justify-start items-start w-full gap-3 p-4 border border-gray-600  border-opacity-50 rounded-xl shadow-2xl">
               <div className='flex items-center gap-3'>
@@ -18,27 +19,40 @@ const Chat = () => {
               </div>
               <div className='flex flex-col items-start w-full gap-3'>
                 <div className='flex justify-between items-center w-full'>
-                <p className='text-[16px]'>Ryan Lee</p>
-                <IoCopyOutline />
+                  <p className='text-[16px]'>Ryan Lee</p>
+                  <IoCopyOutline />
                 </div>
                 <p className='text-[16px] text-gray-400 '>{recentPrompt}</p>
 
               </div>
             </div>
 
-            <div className="user bg-[#0D0F10] flex  justify-start items-start w-full gap-3 p-4 border border-gray-600  border-opacity-50 rounded-xl shadow-2xl">
-              <div className='flex items-center gap-3'>
-                <div className='w-10 h-10 rounded-2xl bg-green-300'></div>
-              </div>
-              <div className='flex flex-col items-start w-full gap-3'>
-                <div className='flex justify-between items-center w-full'>
-                <p className='text-[16px]'>Artificium</p>
-                <IoCopyOutline />
-                </div>
-                <p className='text-[16px] text-gray-400 text-start' dangerouslySetInnerHTML={{__html:data}}></p>
 
-              </div>
-            </div>
+
+            {
+              !loading ? (<>
+                {
+                  data && <div className="user bg-[#0D0F10] flex  justify-start items-start w-full gap-3 p-4 border border-gray-600  border-opacity-50 rounded-xl shadow-2xl">
+                    <div className='flex items-center gap-3'>
+                      <div className='w-10 h-10 rounded-2xl bg-green-300'></div>
+                    </div>
+                    <div className='flex flex-col items-start w-full gap-3'>
+                      <div className='flex justify-between items-center w-full'>
+                        <p className='text-[16px]'>Artificium</p>
+                        <IoCopyOutline />
+                      </div>
+                      <p className='text-[16px] text-gray-400 text-start' dangerouslySetInnerHTML={{ __html: data }}></p>
+
+                    </div>
+                  </div>
+                }
+
+              </>) : (<div>
+            <img className='w-12 grayscale' src={LoadingGIF} alt="loading" />
+              </div>)
+            }
+
+
 
           </div>
         </div>
