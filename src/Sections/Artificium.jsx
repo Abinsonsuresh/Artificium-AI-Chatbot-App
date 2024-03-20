f76a203c0a2f17dc36e082841536455dc3432f73
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-21 01:04:33 +0530

    page load elems added

--- src/Sections/Artificium.jsx
+++ src/Sections/Artificium.jsx
@@ -26,7 +26,114 @@ const Artificium = () => {
                             {/* CONTENT */}
                             <div className='flex w-full  flex-col items-center mt-2 gap-5'>
 
-                                <div className="user bg-[#0D0F10] flex  justify-start items-start w-full gap-3 p-4 border border-gray-600  border-opacity-50 rounded-xl shadow-2xl">
+                                {!data ? (<div>
+                                    <div className='text-center flex flex-col items-center h-full'>
+                                        <div>
+                                            <h3 className='font-bold text-xl'>Innovation Starter Pack</h3>
+                                            <p className='text-sm text-gray-500'>Kickstart your innovation process with our comprehensive selection of predefined prompts.</p>
+                                        </div>
+                                        {/* CONTENT */}
+                                        <div className="flex flex-wrap justify-center items-center gap-5 mt-2">
+                                            <div className='flex flex-col items-center'>
+                                                <div className='flex items-center flex-col my-4'>
+                                                    <div className='bg-gray-900 shadow-2xl shadow-green-500 p-4 rounded-full'>
+                                                        <PiChatCircleLight color='green ' />
+                                                    </div>
+                                                    <span className='font-bold'>Creative Assets</span>
+                                                </div>
+
+                                                {
+                                                    Creativeassets.map((item, index) => {
+                                                        return (
+                                                            <div key={index} className='bg-[#0D0F10] to-black bg-gradient-to-r p-4 w-48 mt-2 rounded-2xl flex justify-between items-center'>
+                                                                <div>
+                                                                    <p>{item.name}</p>
+                                                                </div>
+                                                                <IoIosArrowRoundForward size={21} color='gray' />
+                                                            </div>
+                                                        )
+                                                    })
+                                                }
+                                            </div>
+
+
+                                            <div className='flex flex-col items-center'>
+                                                <div className='flex items-center flex-col my-4'>
+                                                    <div className='bg-gray-900 shadow-2xl shadow-blue-500 p-4 rounded-full'>
+                                                        <FaCode />
+                                                    </div>
+                                                    <span className='font-bold' >Developer Tools</span>
+                                                </div>
+                                                {
+                                                    DeveloperTools.map((item, index) => {
+                                                        return (
+                                                            <div key={index} className='bg-[#0D0F10] to-black bg-gradient-to-r p-4 w-48 mt-2 rounded-2xl flex justify-between items-center'>
+                                                                <div>
+                                                                    <p>{item.name}</p>
+                                                                </div>
+                                                                <IoIosArrowRoundForward size={21} color='gray' />
+                                                            </div>
+                                                        )
+                                                    })
+                                                }
+
+                                            </div>
+
+
+                                            <div className='flex flex-col items-center'>
+                                                <div className='flex items-center flex-col my-4'>
+                                                    <div className='bg-gray-900 shadow-2xl shadow-violet-500 p-4 rounded-full'>
+                                                        <FaPenFancy />
+                                                    </div>
+                                                    <span className='font-bold' >Content Creation</span>
+                                                </div>
+
+                                                {
+                                                    ContentCreation.map((item, index) => {
+                                                        return (
+                                                            <div key={index} className='bg-[#0D0F10] to-black bg-gradient-to-r p-4 w-48 mt-2 rounded-2xl flex justify-between items-center'>
+                                                                <div>
+                                                                    <p>{item.name}</p>
+                                                                </div>
+                                                                <IoIosArrowRoundForward size={21} color='gray' />
+                                                            </div>
+                                                        )
+                                                    })
+                                                }
+                                            </div>
+
+
+
+                                            <div className='flex flex-col items-center'>
+                                                <div className='flex items-center flex-col my-4'>
+                                                    <div className='bg-[#0D0F10] shadow-2xl shadow-yellow-500 p-4 rounded-full'>
+                                                        <FaLightbulb />
+                                                    </div>
+                                                    <span className='font-bold' >Idea Generation</span>
+                                                </div>
+
+                                                {
+                                                    IdeaGeneration.map((item, index) => {
+                                                        return (
+                                                            <div key={index} className='bg-[#0D0F10] p-4 w-48 mt-2 rounded-2xl flex justify-between items-center'>
+                                                                <div>
+                                                                    <p>{item.name}</p>
+                                                                </div>
+                                                                <IoIosArrowRoundForward size={21} color='gray' />
+                                                            </div>
+                                                        )
+                                                    })
+                                                }
+                                            </div>
+                                        </div>
+
+                                        <div className='flex flex-col items-center mt-8'>
+                                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus cumque, molestiae amet sequi totam in quaerat molestias repellat consectetur dolorem eius rerum, enim dolore accusamus. Accusamus autem doloremque nulla.</p>
+
+
+                                        </div>
+                                    </div>
+                                </div>) : (<div className="user bg-[#0D0F10] flex  justify-start items-start w-full gap-3 p-4 border border-gray-600  border-opacity-50 rounded-xl shadow-2xl">
                                     <div className='flex items-center gap-3'>
                                         <div className='w-10 h-10 rounded-2xl bg-red-950'></div>
                                     </div>
@@ -38,14 +145,14 @@ const Artificium = () => {
                                         {/* <p className='text-[16px] text-gray-400 '>{recentPrompt}</p> */}
 
                                     </div>
-                                </div>
+                                </div>)}
 
 
 
                                 {
                                     !loading ? (<>
                                         {
-                                           data && <div className="user bg-[#0D0F10] flex  justify-start items-start w-full gap-3 p-4 border border-gray-600  border-opacity-50 rounded-xl shadow-2xl">
+                                            data && <div className="user bg-[#0D0F10] flex  justify-start items-start w-full gap-3 p-4 border border-gray-600  border-opacity-50 rounded-xl shadow-2xl">
                                                 <div className='flex items-center gap-3'>
                                                     <div className='w-10 h-10 rounded-2xl bg-green-300'></div>
                                                 </div>
@@ -76,7 +183,7 @@ const Artificium = () => {
 
             <div className="p-2">
                 <div className="p-4 bg-[#0D0F10] rounded-xl shadow-lg  flex relative ">
-                    <input onChange={(e) => setInput(e.target.value)}  className="input flex-1 text-white focus:outline-none bg-transparent rounded-r-none" type="text" />
+                    <input onChange={(e) => setInput(e.target.value)} className="input flex-1 text-white focus:outline-none bg-transparent rounded-r-none" type="text" />
                     <PiArrowFatLinesRight onClick={() => onSent()} className='absolute top-[30%] right-10 cursor-pointer' size={20} />
                 </div>
             </div>
