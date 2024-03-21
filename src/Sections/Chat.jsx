fd03c79c6ff1a256ba34472a9bec6c45292e2760
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-22 01:28:12 +0530

    AI imgs aded

--- src/Sections/Chat.jsx
+++ src/Sections/Chat.jsx
@@ -35,8 +35,10 @@ const Chat = () => {
                       <IoCopyOutline />
                     </div>
 
-                    <div>
-                      <img className='w-96 rounded-2xl' src="https://wp.technologyreview.com/wp-content/uploads/2022/09/greg-rutkowski-dragon-cave-square.jpg" alt="" />
+                    <div className='flex items-center gap-5 flex-col md:flex-row'>
+                    <img className='w-96 rounded-2xl' src="https://wp.technologyreview.com/wp-content/uploads/2022/09/greg-rutkowski-dragon-cave-square.jpg" alt="" />
+
+                      <img className='min-w-96 min-h-96 rounded-2xl object-cover' src="https://img.freepik.com/free-photo/spooky-night-dark-mystery-fantasy-landscape-illuminated-lantern-autumn-colors-generated-by-artificial-intelligence_24640-131066.jpg" alt="" />
                     </div>
 
                     <p>Generate AI Images</p>
