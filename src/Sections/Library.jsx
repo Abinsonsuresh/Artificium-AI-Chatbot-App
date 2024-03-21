b56f7df7ae0cc51d1831ed51c0a5eec344d444bf
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-21 23:05:00 +0530

    Elems arranged

--- src/Sections/Library.jsx
+++ src/Sections/Library.jsx
@@ -4,82 +4,9 @@ import { PiChatCircleLight } from "react-icons/pi";
 const Library = () => {
     return (
         <>
-            <div className="flex w-full flex-wrap  justify-center items-center h-full border gap-5  mt-2 p-2 overflow-y-scroll  scroll  scroll-smooth">
+            <div className="flex w-full flex-wrap  justify-center items-center h-full border gap-5  mt-2 p-2 overflow-y-scroll  scroll  scroll-smooth hide-scrollbar">
 
-                <div className='ImagesSection border border-opacity-10 border-gray-600 w-full max-w-[300px] h-full rounded-2xl p-4 flex flex-col justify-between gap-5 overflow-y-scroll  scroll  scroll-smooth'>
-
-                    <div className="h my-6">
-                        <p className='text-xl font-bold'>Images</p>
-                    </div>
-
-                    <div className='card flex flex-col items-center'>
-
-                        <div className='bg-[#0D0F10] h-full flex flex-col items-center rounded-2xl justify-start'>
-                            <div className='p-4 text-start font-semibold'>
-                                <p>Captain Drake</p>
-                                <p>Natural born leader with years of experience in space exploration.</p>
-                            </div>
-                            <div className='my-4 w-full p-2 '>
-                                <img className=' object-cover object-center rounded-xl' src="https://img.freepik.com/free-photo/night-sky-glows-with-galaxy-mystical-silhouette-generative-ai_188544-11287.jpg?size=626&ext=jpg&ga=GA1.1.735520172.1710892800&semt=ais" alt="" />
-                            </div>
-
-                        </div>
-
-                        <div className=' bg-[#0D0F10] p-8 my-2 w-full rounded-xl flex items-center justify-between'>
-                            <div>
-                                <div className="flex -space-x-4 rtl:space-x-reverse">
-                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
-                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
-                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
-                                    <a className="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800" href="#">+99</a>
-                                </div>
-                            </div>
-
-                            <div className='flex items-center gap-3'>
-                                <p>12</p>
-                                <PiChatCircleLight color='green' />
-
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div className='card flex flex-col items-center'>
-
-                        <div className='bg-[#0D0F10] h-full flex flex-col items-center rounded-2xl justify-start'>
-                            <div className='p-4 text-start font-semibold'>
-                                <p>Captain Drake</p>
-                                <p>Natural born leader with years of experience in space exploration.</p>
-                            </div>
-                            <div className='my-4 w-full p-2 '>
-                                <img className=' object-cover object-center rounded-xl' src="https://img.freepik.com/free-photo/night-sky-glows-with-galaxy-mystical-silhouette-generative-ai_188544-11287.jpg?size=626&ext=jpg&ga=GA1.1.735520172.1710892800&semt=ais" alt="" />
-                            </div>
-
-                        </div>
-
-                        <div className=' bg-[#0D0F10] p-8 my-2 w-full rounded-xl flex items-center justify-between'>
-                            <div>
-                                <div className="flex -space-x-4 rtl:space-x-reverse">
-                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
-                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
-                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
-                                    <a className="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800" href="#">+99</a>
-                                </div>
-                            </div>
-
-                            <div className='flex items-center gap-3'>
-                                <p>12</p>
-                                <PiChatCircleLight color='green' />
-
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
-
-
-                <div className='ImagesSection border border-opacity-10 border-gray-600 w-full max-w-[300px] h-full rounded-2xl p-4 flex flex-col justify-between gap-5 overflow-y-scroll  scroll  scroll-smooth'>
+                <div className='ImagesSection border border-opacity-10 border-gray-600 w-full max-w-[300px] h-full rounded-2xl p-4 flex flex-col justify-between gap-5 overflow-y-scroll  scroll  scroll-smooth hide-scrollbar'>
 
                     <div className="h my-6">
                         <p className='text-xl font-bold'>Images</p>
@@ -152,10 +79,10 @@ const Library = () => {
                 </div>
 
 
-                <div className='ImagesSection border border-opacity-10 border-gray-600 w-full max-w-[300px] h-full rounded-2xl p-4 flex flex-col justify-between gap-5 overflow-y-scroll  scroll  scroll-smooth'>
+                <div className='ImagesSection border border-opacity-10 border-gray-600 w-full max-w-[300px] h-full rounded-2xl p-4 flex flex-col justify-between gap-5 overflow-y-scroll  scroll  scroll-smooth hide-scrollbar'>
 
                     <div className="h my-6">
-                        <p className='text-xl font-bold'>Images</p>
+                        <p className='text-xl font-bold'>Documents</p>
                     </div>
 
                     <div className='card flex flex-col items-center'>
@@ -165,9 +92,7 @@ const Library = () => {
                                 <p>Captain Drake</p>
                                 <p>Natural born leader with years of experience in space exploration.</p>
                             </div>
-                            <div className='my-4 w-full p-2 '>
-                                <img className=' object-cover object-center rounded-xl' src="https://img.freepik.com/free-photo/night-sky-glows-with-galaxy-mystical-silhouette-generative-ai_188544-11287.jpg?size=626&ext=jpg&ga=GA1.1.735520172.1710892800&semt=ais" alt="" />
-                            </div>
+                        
 
                         </div>
 
@@ -197,9 +122,75 @@ const Library = () => {
                                 <p>Captain Drake</p>
                                 <p>Natural born leader with years of experience in space exploration.</p>
                             </div>
-                            <div className='my-4 w-full p-2 '>
-                                <img className=' object-cover object-center rounded-xl' src="https://img.freepik.com/free-photo/night-sky-glows-with-galaxy-mystical-silhouette-generative-ai_188544-11287.jpg?size=626&ext=jpg&ga=GA1.1.735520172.1710892800&semt=ais" alt="" />
+                        
+                        </div>
+
+                        <div className=' bg-[#0D0F10] p-8 my-2 w-full rounded-xl flex items-center justify-between'>
+                            <div>
+                                <div className="flex -space-x-4 rtl:space-x-reverse">
+                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
+                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
+                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
+                                    <a className="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800" href="#">+99</a>
+                                </div>
                             </div>
+
+                            <div className='flex items-center gap-3'>
+                                <p>12</p>
+                                <PiChatCircleLight color='green' />
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+                <div className='ImagesSection border border-opacity-10 border-gray-600 w-full max-w-[300px] h-full rounded-2xl p-4 flex flex-col justify-between gap-5 overflow-y-scroll  scroll  scroll-smooth hide-scrollbar'>
+
+                    <div className="h my-6">
+                        <p className='text-xl font-bold'>Ideas</p>
+                    </div>
+
+                    <div className='card flex flex-col items-center'>
+
+                        <div className='bg-[#0D0F10] h-full flex flex-col items-center rounded-2xl justify-start'>
+                            <div className='p-4 text-start font-semibold'>
+                                <p>Captain Drake</p>
+                                <p>Natural born leader with years of experience in space exploration.</p>
+                            </div>
+                         
+
+                        </div>
+
+                        <div className=' bg-[#0D0F10] p-8 my-2 w-full rounded-xl flex items-center justify-between'>
+                            <div>
+                                <div className="flex -space-x-4 rtl:space-x-reverse">
+                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
+                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
+                                    <img className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg" alt="" />
+                                    <a className="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800" href="#">+99</a>
+                                </div>
+                            </div>
+
+                            <div className='flex items-center gap-3'>
+                                <p>12</p>
+                                <PiChatCircleLight color='green' />
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className='card flex flex-col items-center'>
+
+                        <div className='bg-[#0D0F10] h-full flex flex-col items-center rounded-2xl justify-start'>
+                            <div className='p-4 text-start font-semibold'>
+                                <p>Captain Drake</p>
+                                <p>Natural born leader with years of experience in space exploration.</p>
+                            </div>
+                          
 
                         </div>
 
